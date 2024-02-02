7a451c67ea494e8efab39463dbf103043beaadb8
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-03 00:23:28 +0300

    config rect query, axios and next auth

---
+++ src/lib/session.provider.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import {SessionProvider} from "next-auth/react"
+import {getServerSession} from "next-auth";
+export default async function AuthSessionProvider({children}: { children: React.ReactNode }) {
+    const session = await getServerSession()
+    return (
+        <SessionProvider session={session}>
+            {children}
+        </SessionProvider>
+    )
+}

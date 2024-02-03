197620decbd7f0d7c5088ca9a8fb1b2dbb0adb4a
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-03 04:30:04 +0300

    feature: "create endpoints file for project management"

---
+++ src/app/managment/_constants/endpoints.ts
@@ -0,0 +1,18 @@
+
+
+export enum WORKSPACE_ENDPOINTS{
+    CREATE = "",
+    GET_BY_ID = "",
+    RECENT_LIST=""
+}
+export enum TASK_ENDPOINTS{
+    CREATE="",
+    UPDATE_STATUS=""
+}
+
+export enum MESSAGE_ENDPOINTS{
+    CREATE="",
+    LIST="",
+    DELETE="",
+    UPDATE=""
+}

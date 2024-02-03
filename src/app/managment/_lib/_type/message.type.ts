16c62bb49cc0531b339e9a38bec4c65785b80b3d
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-03 04:32:48 +0300

    feature: "add message type"

--- src/app/managment/_lib/_type/message.type.ts
+++ src/app/managment/_lib/_type/message.type.ts
@@ -1,6 +1,10 @@
 export interface IMessage{
     messageId:string
     date:string
-    text:string
+    text:string,
     userId:string
+}
+export interface IMessageToCreate{
+    userId:string,
+    text:string
 }

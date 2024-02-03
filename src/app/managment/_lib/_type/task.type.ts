ab2d02f927a237cce09db4b86f95d6fdbcdda7be
Finoana7 <finoana.marinah@gmail.com>
2024-02-03 03:04:15 +0300

    workspace modifié

--- src/app/managment/_lib/_type/task.type.ts
+++ src/app/managment/_lib/_type/task.type.ts
@@ -1,28 +1,21 @@
 export interface ITask{
-    taskId:string,
-    descriptionTask:string,
+    taskId:string
+    descriptionTask:string
     taskTitle:string
 }
 
-export interface IHistory{
-    historyId:string,
-    workspaceId:string,
-    description:string
-}
-
 export interface ILink{
-    taskId:string,
+    taskId:string
     tagId:string
 }
 
-export interface IWorkspaceToCreate{
-    workspaceId: string,
-    workspaceTitle: string,
-    userId:string
+export interface ITaskToCreate{
+    descriptionTask:string
+    taskTitle:string
+    workspaceId:string
 }
 
-export interface ITaskToCreate{
-    descriptionTask:string,
-    taskTitle:string,
-    workspaceId:string
+export interface ITag{
+    tagId:string
+    description:string
 }
--- src/app/managment/_lib/_type/types.ts
+++ src/app/managment/_lib/_type/types.ts
@@ -1,9 +1,21 @@
 import { ITask } from "./task.type";
 
 export interface IWorkspace{
-    workspaceId: string,
-    workspaceTitle: string,
-    userId:string,
+    workspaceId: string
+    workspaceTitle: string
+    userId:string
     taskList:ITask[]
+}
+
+export interface IWorkspaceToCreate{
+    workspaceId: string
+    workspaceTitle: string
+    userId:string
+}
+
+export interface IHistory{
+    historyId:string
+    workspaceId:string
+    description:string
 }
 

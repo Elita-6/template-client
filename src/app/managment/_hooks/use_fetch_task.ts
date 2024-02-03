b891eb3564003f78939c3ec663b9b9da163b29bc
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-03 04:32:30 +0300

    feature: "add fetch task hooks"

--- src/app/managment/_hooks/use_fetch_task.ts
+++ src/app/managment/_hooks/use_fetch_task.ts
@@ -1,1 +1,10 @@
+import {useQuery} from "react-query";
+import {workspaceService} from "@/app/managment/_services/workspace.service";
 
+
+export const useFetchStatus= (workspaceId:string) =>{
+    return useQuery({
+        queryKey:['status'],
+        queryFn:()=> workspaceService.getTaskListByWorkspaceId(workspaceId)
+    })
+}

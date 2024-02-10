30a632e3e95f92c3913ebbdcbe9f3df3cab55623
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-10 08:06:48 +0300

    feature: add "add button" for task

---
+++ src/app/dashboard/workspace/_components/button/button_add.tsx
@@ -0,0 +1,14 @@
+'use client'
+import * as React from 'react';
+import { IoAddOutline } from "react-icons/io5";
+
+type Props = {
+    HandleClick: ()=> void
+};
+export const ButtonAdd = (props: Props) => {
+    return (
+        <button onClick={props.HandleClick} className=' rounded-lg flex justify-center border  border-[#E2E8F0] bg-white w-full p-2'>
+            <IoAddOutline/>
+        </button>
+    );
+};

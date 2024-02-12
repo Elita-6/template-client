6f9defd26d2d123631c5148061a0da50f95c0cf1
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-12 16:52:53 +0300

    feature: make task list dynamic

--- src/app/dashboard/workspace/[slug]/page.tsx
+++ src/app/dashboard/workspace/[slug]/page.tsx
@@ -1,6 +1,9 @@
 'use client'
 import {TaskCard} from "@/app/dashboard/workspace/_components/kanban/task_card";
 import {ButtonAdd} from "@/app/dashboard/workspace/_components/button/button_add";
+import {allTasks} from "@/app/dashboard/_constant/data.example";
+import {ITaskGroup} from "@/app/managment/_lib/_type/task.types";
+import clsx from "clsx";
 
 
 export default function WorkspaceDetails({params}:{params:{slug:string}}){
@@ -11,36 +14,25 @@ export default function WorkspaceDetails({params}:{params:{slug:string}}){
             <div className="space-y-5 w-full">
                 <div className='bg-[#e6eaf042] w-full py-5 space-y-5 '>
                     <div className='flex w-full flex-wrap justify-between '>
-                        <div className='w-[30%] flex flex-col space-y-10'>
-                            <div className='flex flex-col space-y-3'>
-                                <div className='flex  space-x-2 items-center'>
-                                    <div className='bg-[#f6e549] rounded-full w-2 h-2'/>
-                                    <span className='text-sm opacity-75'>Waiting</span>
-                                </div>
-                                <ButtonAdd HandleClick={HandleClick}/>
-                            </div>
-                            <TaskCard/>
-                        </div>
-                        <div className='w-[30%] flex flex-col space-y-10'>
-                            <div className='flex flex-col space-y-3'>
-                                <div className='flex  space-x-2 items-center'>
-                                    <div className='bg-[#f6e549] rounded-full w-2 h-2'/>
-                                    <span className='text-sm opacity-75'>Waiting</span>
-                                </div>
-                                <ButtonAdd HandleClick={HandleClick}/>
-                            </div>
-                            <TaskCard/>
-                        </div>
-                        <div className='w-[30%] flex flex-col space-y-10'>
-                            <div className='flex flex-col space-y-3'>
-                                <div className='flex  space-x-2 items-center'>
-                                    <div className='bg-[#f6e549] rounded-full w-2 h-2'/>
-                                    <span className='text-sm opacity-75'>Waiting</span>
-                                </div>
-                                <ButtonAdd HandleClick={HandleClick}/>
-                            </div>
-                            <TaskCard/>
-                        </div>
+                        {
+                            allTasks.map((value:ITaskGroup,key)=>(
+                                    <div className='w-[30%] flex flex-col space-y-10' key={key}>
+                                        <div className='flex flex-col space-y-3'>
+                                            <div className='flex  space-x-2 items-center'>
+                                                <div className={`bg-[#${value.color}]`}/>
+                                                <span className='text-sm opacity-75'>{value.status} {value.color} </span>
+                                            </div>
+                                            <ButtonAdd HandleClick={HandleClick}/>
+                                        </div>
+                                        {
+                                            value.tasks.map((value,key:number)=>
+                                                <TaskCard task={value} key={key}/>
+                                            )
+                                        }
+                                    </div>
+                                )
+                            )
+                        }
                     </div>
                     <div>
 

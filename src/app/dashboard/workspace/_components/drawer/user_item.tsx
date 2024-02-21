2fb2e1de60ce993fc5bdc3eff463439db20d90e3
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-21 08:35:04 +0300

    refactor : remove unused import

--- src/app/dashboard/workspace/_components/drawer/user_item.tsx
+++ src/app/dashboard/workspace/_components/drawer/user_item.tsx
@@ -1,4 +1,4 @@
-import React, {MouseEventHandler, useState} from 'react';
+import React, {useState} from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 interface Props{

78905df6a24b896b3e70e4605954b7b904dfcaae
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-22 18:24:39 +0300

    feature: make task list dynamic

--- src/app/dashboard/workspace/_components/kanban/kanban_container.tsx
+++ src/app/dashboard/workspace/_components/kanban/kanban_container.tsx
@@ -6,11 +6,13 @@ import {ITaskGroup} from "@/app/managment/_lib/_type/task.types";
 import RoundedIndicator from "@/app/dashboard/workspace/_components/kanban/rounded_indicator";
 import {ButtonAdd} from "@/app/dashboard/workspace/_components/button/button_add";
 import {TaskCard} from "@/app/dashboard/workspace/_components/kanban/task_card";
+import {useWorkspaceDetailsStore} from "@/app/state/global_state";
 
 const KanbanContainer = () => {
     const HandleClick = ()=>{
         setIsOPen(oldValue=>!oldValue)
     }
+    const taskDetails = useWorkspaceDetailsStore((state)=>state.taskDetails)
     const [isOpen,setIsOPen] = useState<boolean>(false)
     return (
         <div>
@@ -23,7 +25,7 @@ const KanbanContainer = () => {
                 <div className='bg-[#e6eaf042] w-full py-5 space-y-5 '>
                     <div className='flex w-full flex-wrap justify-between '>
                         {
-                            allTasks.map((value:ITaskGroup,key)=>(
+                            taskDetails.map((value:ITaskGroup,key)=>(
                                     <div className='w-[30%] flex flex-col space-y-6' key={key}>
                                         <div className='flex flex-col space-y-3'>
                                             <div className='flex  space-x-2 items-center'>

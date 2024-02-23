37a081dd4ff15b6dce1a3f2133a66d88dea0f540
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-02-23 15:29:11 +0300

    add: overview page

--- src/app/dashboard/workspace/(task_overview)/components/card/card.tsx
+++ src/app/dashboard/workspace/(task_overview)/components/card/card.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Progress} from "@/components/ui/progress";
 
 const Card = () => {
     return (
-        <div>
-            
+        <div className="flex flex-col gap-5 bg-white h-32 w-100 p-4">
+            <span className="inline bg-green-300 p-2 px-4 rounded-full text-gray-700">
+                Working on
+            </span>
+            <div>
+                <div>
+                    <div className="flex justify-between">
+                        <span>66%</span>
+                        <span>12/24</span>
+                    </div>
+                    <Progress value={66} />
+                </div>
+            </div>
         </div>
     );
 };
--- src/app/dashboard/workspace/[slug]/page.tsx
+++ src/app/dashboard/workspace/[slug]/page.tsx
@@ -6,6 +6,7 @@ import {ITaskGroup} from "@/app/managment/_lib/_type/task.types";
 import {useState} from "react";
 import CreateTaskModal from "@/app/dashboard/workspace/_components/modal/create_task_modal";
 import RoundedIndicator from "@/app/dashboard/workspace/_components/kanban/rounded_indicator";
+import Card from "@/app/dashboard/workspace/(task_overview)/components/card/card";
 
 
 
@@ -16,40 +17,9 @@ export default function WorkspaceDetails({params}:{params:{slug:string}}){
     const [isOpen,setIsOPen] = useState<boolean>(false)
     return(
         <div className="">
-            {
-                isOpen && (
-                    <CreateTaskModal HandleClick={HandleClick}/>
-                )
-            }
-            <div className="space-y-5 w-full">
-                <div className='bg-[#e6eaf042] w-full py-5 space-y-5 '>
-                    <div className='flex w-full flex-wrap justify-between '>
-                        {
-                            allTasks.map((value:ITaskGroup,key)=>(
-                                    <div className='w-[30%] flex flex-col space-y-10' key={key}>
-                                        <div className='flex flex-col space-y-3'>
-                                            <div className='flex  space-x-2 items-center'>
-                                                <RoundedIndicator color={value.color}/>
-                                                <span className='text-sm opacity-75'>{value.status} </span>
-                                            </div>
-                                            <ButtonAdd HandleClick={HandleClick}/>
-                                        </div>
-                                        {
-                                            value.tasks.map((value,key:number)=>
-                                                <TaskCard task={value} key={key}/>
-                                            )
-                                        }
-                                    </div>
-                                )
-                            )
-                        }
-                    </div>
-                    <div>
-
-                    </div>
-                </div>
+            <div className="grid grid-cols-6">
+                <Card />
             </div>
-
         </div>
     )
 }

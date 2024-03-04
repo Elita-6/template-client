c83bb7c90d441e00d7b5454e4e85ef14eb11637a
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-04 08:18:18 +0300

    fix : should fix production redering error

--- src/app/dashboard/workspace/[slug]/page.tsx
+++ src/app/dashboard/workspace/[slug]/page.tsx
@@ -1,27 +1,11 @@
-"use client"
-import KanbanContainer from "@/app/dashboard/workspace/_components/kanban/kanban_container";
 import {useSearchParams} from "next/navigation";
-import Overview from "@/app/dashboard/workspace/(task_overview)/components/view/overview";
+import SlugContainer from "@/app/dashboard/workspace/_component/slug_container";
 
 
 export default function WorkspaceDetails({params}:{params:{slug:string}}){
     const searchParams = useSearchParams();
     const section = new URLSearchParams(searchParams).get('section')
-    const currentPage = () =>{
-        switch(section) {
-            case "tasks":
-                return <KanbanContainer />
-            case "files":
-                break
-            case "overview":
-                return <Overview />
-            default:
-                return <KanbanContainer />
-        }
-    }
     return(
-        <div className="h-full">
-            {currentPage()}
-        </div>
+        <SlugContainer section={section!}/>
     )
 }
